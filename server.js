d4b1847ee8cc734ab2eb4d69f5243087e65494f5
sauryasaurya <saurya.bhandari78792@gmail.com>
2024-06-19 14:28:41 +0545

    multiple routes

--- server.js
+++ server.js
@@ -4,10 +4,20 @@ const path = require("path");
 
 const PORT = process.env.PORT || 3500;
 
-app.get("/", (req, res) => {
-  // res.send("Hello World!!");
-  // res.sendFile("./views/index.html", { root: __dirname });
+app.get("^/$|/index(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+
+app.get("/old-page(.html)?", (req, res) => {
+  res.redirect(301, "/new-page");
+});
+
+app.get("/*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
