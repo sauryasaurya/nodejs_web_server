7f677a58d71f4db4adbf57a7fc005984225ad9b6
sauryasaurya <saurya.bhandari78792@gmail.com>
2024-06-25 12:51:28 +0545

    employee data

--- server.js
+++ server.js
@@ -47,6 +47,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/root"));
 app.use("/subdir", require("./routes/subdir"));
+app.use("/employees", require("./routes/api/employees"));
 
 app.get("/*", (req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
