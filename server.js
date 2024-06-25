e0a838fe742d3db7f87dce79ec561e59b4842be9
sauryasaurya <saurya.bhandari78792@gmail.com>
2024-06-25 09:48:47 +0545

    add 404 for all type of requests

--- server.js
+++ server.js
@@ -87,6 +87,17 @@ app.get("/*", (req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
+app.all("*", (req, res) => {
+  res.status(400);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
